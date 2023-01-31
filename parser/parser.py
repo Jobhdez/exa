63415ffcfdd9a504a36911a3ac4bca40f706c50b
Job Hernandez <hj93@protonmail.com>
2023-01-31 12:29:41 -0800

    first commit

---
+++ parser/parser.py
@@ -0,0 +1,114 @@
+import lark
+
+grammar = '''
+start  : _exprs
+_exprs : _e* _e
+_e     : if
+       | let
+       | ATOM
+       | _num
+       | BOOL
+       | list
+TRUE   : "#t"
+FALSE  : "#f"
+BOOL   : TRUE | FALSE
+list   : "(" _exprs? ")"
+let    : "(" "let" "(" binding+ ")" _exprs ")"
+if     : "(" "if" _e _e _e ")"
+binding : "(" ATOM _e ")"
+INT    : /[-+]?[0-9]+/
+ATOM   : /[a-zA-Z]+[a-zA-Z0-9\-\?\!]*/
+       | /[\*\/\=\>\<]/
+       | /[\-\+](?![0-9])/
+FLOAT  : /[-+]?[0-9]+\.[0-9]*/
+_num   : INT | FLOAT
+%import common.WS
+%import common.NEWLINE
+%ignore WS
+    '''
+# keyword : /[a-z]+/
+parser = lark.Lark(grammar)
+
+
+## Nodes
+class If:
+    def __init__(self, condition, scmthen, scmelse):
+        self.condition = condition
+        self.scmthen = scmthen
+        self.scmelse = scmelse
+
+    def __repr__(self):
+        return f'(IF (Condition {self.condition}) (Then {self.scmthen}) (Else {self.scmelse}))'
+
+class List:
+    def __init__(self, expressions):
+        self.expressions = expressions
+
+    def __repr__(self):
+        return f'(List {self.expressions})'
+
+class Atom:
+    def __init__(self, atom):
+        self.atom = atom
+
+    def __repr__(self):
+        return f'(Atom {self.atom})'
+
+class Int:
+    def __init__(self, num):
+        self.num = num
+
+    def __repr__(self):
+        return f'(Int {self.num})'
+
+class Let:
+    def __init__(self, bindings, body):
+        self.bindings = bindings
+        self.body = body
+
+    def __repr__(self):
+        return f'(Let (Binding {self.bindings}) (Body {self.body}))'
+
+class Binding:
+    def __init__(self, bindings):
+        self.bindings = bindings
+
+    def __repr__(self):
+        return f'{self.bindings}'
+
+def make_parse_tree(tree):
+    match tree:
+        case x if isinstance(x, lark.tree.Tree):
+            if tree.data == 'start':
+                return [make_parse_tree(x) for x in tree.children]
+            elif tree.data == 'if':
+                if_exps = [make_parse_tree(x) for x in tree.children]
+                if_cond = if_exps[0]
+                if_then = if_exps[1]
+                if_else = if_exps[2]
+                return If(if_cond, if_then, if_else)
+            elif tree.data == 'let':
+                let_exps = [make_parse_tree(x) for x in tree.children]
+                if len(let_exps) == 2:
+                    let_bindings = let_exps[0]
+                    let_body = let_exps[1]
+                    return Let(let_bindings, let_body)
+                else:
+                    length = len(let_exps)
+                    let_bindings = let_exps[:length-1]
+                    let_body = let_exps[length-1]
+                    return Let(let_bindings, let_body)
+            elif tree.data == 'binding':
+                return Binding([make_parse_tree(x) for x in tree.children])
+            else:
+                return List([make_parse_tree(x) for x in tree.children])
+
+        case x if isinstance(x, lark.lexer.Token):
+            ty = tree.type.lower()
+            if ty == 'atom':
+                return Atom(tree.value)
+
+            else:
+                return Int(tree.value)
+
+        
