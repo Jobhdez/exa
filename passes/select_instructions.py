6eac6543ca4975a4637a9443d64e5a7e5c9b7a53
Job Hernandez <hj93@protonmail.com>
2023-02-04 10:09:54 -0800

    add first case.

--- passes/select_instructions.py
+++ passes/select_instructions.py
@@ -3,6 +3,12 @@ from passes.uniquify import *
 from passes.explicate_control import *
 from parser.parser import *
 
+class AssemblyProgram:
+    def __init__(self, instructions):
+        self.instructions = instructions
+
+    def __repr__(self):
+        return f'(AssemblyProgram {self.instructions})'
 
 class Instruction:
     def __init__(self, instr, param1, param2):
@@ -19,3 +25,48 @@ class Immediate:
         self.num = num
     def __repr__(self):
         return f'(Immediate {self.num})'
+
+
+class Register:
+    def __init__(self, reg):
+        self.reg = reg
+
+    def __repr__(self):
+        return f'(Register {self.reg})'
+
+class Retq:
+    def __init__(self, instr):
+        self.instr = instr
+
+    def __repr__(self):
+        return f'(Retq {self.instr})'
+
+
+
+def select_instructions(ast):
+    """
+    makes the assembly instructions explicit.
+
+    @param ast
+    @returns: assembly (x86-64) based ast.
+
+    Example:
+        (+ 2 3)
+
+    """
+
+    match ast:
+        case x if isprim_addition(x):
+            e1 = x.expressions[1]
+            e2 = x.expressions[2]
+            imm = Immediate(e1)
+            imm2 = Immediate(e2)
+            reg = Register('%rax')
+            instr = Instruction('movq', imm, reg)
+            instr2 = Instruction('addq', imm2, reg)
+            instr3 = Retq('retq')
+            assembly_program = []
+            assembly_program.append(instr)
+            assembly_program.append(instr2)
+            assembly_program.append(instr3)
+            return AssemblyProgram(assembly_program)
