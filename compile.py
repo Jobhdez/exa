d7b426727f024548c2856c863f2b0b896de80bee
Job Henandez Lara <hj93@protonmail.com>
2023-03-10 09:08:13 -0800

    Update compile.py

--- compile.py
+++ compile.py
@@ -8,4 +8,4 @@ def compile(expr):
   note: as of 3/9/23 this code has not been tested but I experimented with these functions in the repl while I developed it.
   """
   ast = parse_tree_to_ast(parser.parse(expr)
-  return select_instructions(explicate_control(remove_complex(uniquify(ast,{},0)), 0, {}, {})
+  return select_instructions(explicate_control(remove_complex(uniquify(ast,{},0)), 0, {}, {}))
